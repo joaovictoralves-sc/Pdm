9c3df937e36f633e1bb2b3ce58fcf73388650d42
João Victor Alves <joao.v2007@aluno.ifsc.edu.br>
2025-03-31 08:57:28 -0300

    Comunicação entre atividades, bundle

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ifsc.contaclicks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -34,42 +35,11 @@ public class MainActivity extends AppCompatActivity {
         Button= findViewById(R.id.Button);
 
         Button.setOnClickListener(v -> {
-            //calcular o imc
-            //recuperar os dados anteriormente
-
-            double peso, altura, imc;
-            peso=Double.parseDouble(edpeso.getText().toString());
-            altura= Double.parseDouble((edaltura.getText().toString()));
-            imc=peso/(altura*altura);
-            //formatando numero ##,##
-            DecimalFormat decimalformat = new DecimalFormat("##.##");
-
-            tvresultadoimc.setText(decimalformat.format(imc));
+            Intent intent = new Intent(getApplicationContext(), MainActivityB.class);
+            String msg = edpeso.getText().toString();
+            intent.putExtra("mensagem", msg);
+            startActivity(intent);
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("ciclo de vida", "metodo onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("ciclo de vida", "metodo onResume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("ciclo de vida", "metodo onStop");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("ciclo de vida", "metodo onPause");
-
-    }
 }
