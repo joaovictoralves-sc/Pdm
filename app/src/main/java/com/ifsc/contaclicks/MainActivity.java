0415ebceba1966c55726d7323a0bbbb5d0da4a07
João Victor Alves <joao.v2007@aluno.ifsc.edu.br>
2025-04-14 09:00:25 -0300

    IMC

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -1,45 +1,42 @@
 package com.ifsc.contaclicks;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    int i = 0;
-    EditText edpeso, edaltura;
-    TextView tvresultadoimc;
-    Button Button;
+    EditText edtPeso, edtAltura;
+    Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("ciclo de vida", "metodo onCreate");
         setContentView(R.layout.activity_main);
-        edpeso= findViewById(R.id.edpeso);
-        edaltura= findViewById(R.id.edaltura);
-        tvresultadoimc= findViewById(R.id.tvresultadoimc);
-        Button= findViewById(R.id.Button);
 
-        Button.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), MainActivityB.class);
-            String msg = edpeso.getText().toString();
-            intent.putExtra("mensagem", msg);
-            startActivity(intent);
+        edtPeso = findViewById(R.id.edtPeso);
+        edtAltura = findViewById(R.id.edtAltura);
+        btnCalcular = findViewById(R.id.btnCalcular);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pesoStr = edtPeso.getText().toString();
+                String alturaStr = edtAltura.getText().toString();
+
+                if (!pesoStr.isEmpty() && !alturaStr.isEmpty()) {
+                    double peso = Double.parseDouble(pesoStr);
+                    double altura = Double.parseDouble(alturaStr);
+                    double imc = peso / (altura * altura);
+
+                    Intent intent = new Intent(MainActivity.this, MainActivityB.class);
+                    intent.putExtra("imc", imc);
+                    startActivity(intent);
+                }
+            }
         });
     }
-
 }
--- app/src/main/java/com/ifsc/contaclicks/MainActivityB.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivityB.java
@@ -1,24 +1,46 @@
 package com.ifsc.contaclicks;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivityB extends AppCompatActivity {
+
+    TextView txtResultado;
+    ImageView imagemResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_b);
-        Bundle bundle=getIntent().getExtras();
-        String msg=bundle.getString("mensagem");
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(msg);
 
+        txtResultado = findViewById(R.id.txtResultado);
+        imagemResultado = findViewById(R.id.imagemResultado);
+
+        double imc = getIntent().getDoubleExtra("imc", 0.0);
+        String classificacao;
+
+        if (imc < 18.5) {
+            classificacao = "Abaixo do peso";
+            imagemResultado.setImageResource(R.drawable.abaixopeso);
+        } else if (imc < 24.9) {
+            classificacao = "Peso ideal";
+            imagemResultado.setImageResource(R.drawable.normal);
+        } else if (imc < 29.9) {
+            classificacao = "Sobrepeso";
+            imagemResultado.setImageResource(R.drawable.sobrepeso);
+        } else if (imc < 34.9) {
+            classificacao = "Obesidade grau 1";
+            imagemResultado.setImageResource(R.drawable.obesidade1);
+        } else if (imc < 39.9) {
+            classificacao = "Obesidade grau 2";
+            imagemResultado.setImageResource(R.drawable.obesidade2);
+        } else {
+            classificacao = "Obesidade grau 3";
+            imagemResultado.setImageResource(R.drawable.obesidade3);
+        }
+
+        txtResultado.setText(String.format("IMC: %.2f\n%s", imc, classificacao));
     }
 }
