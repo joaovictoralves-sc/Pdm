3f88a272a033b7c442033ada460295a5903ae83c
João Victor Alves <joao.v2007@aluno.ifsc.edu.br>
2025-05-26 08:15:53 -0300

    Layout criado

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -12,25 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Associando objeto interface a variavel local;
-        TextView tv=findViewById(R.id.TextView);
-        tv.setText(getString(R.string.app_name));
-        Button b=findViewById(R.id.Button);
-        b.setOnClickListener(v -> {//Seu codigo lindo aqui
-        });
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText( Integer.toString(i)  );
-                i++;
-            }
-        });
-
     }
 }
