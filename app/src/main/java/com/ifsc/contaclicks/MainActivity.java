e07102ab0c383771b12230925ea12a760ee0f214
João Victor Alves <joao.v2007@aluno.ifsc.edu.br>
2025-06-16 08:54:09 -0300

    Laucher

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -1,36 +1,44 @@
 package com.ifsc.contaclicks;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    int i=0;
+
+    PackageManager pm;
+    List<ApplicationInfo> applicationInfoList;
+    ListView lv;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Associando objeto interface a variavel local;
-        TextView tv=findViewById(R.id.TextView);
-        tv.setText(getString(R.string.app_name));
-        Button b=findViewById(R.id.Button);
-        b.setOnClickListener(v -> {//Seu codigo lindo aqui
-        });
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText( Integer.toString(i)  );
-                i++;
-            }
-        });
+        lv=findViewById(R.id.listView);
+        pm=getPackageManager();
+
+
+
+        applicationInfoList=pm.getInstalledApplications(PackageManager.MATCH_ALL);
+
+        AppAdapter appAdapter = new AppAdapter(this,R.layout.app_item,applicationInfoList);
+        lv.setAdapter(appAdapter);
+
 
     }
 }
